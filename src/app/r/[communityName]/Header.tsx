263799538e376353ae128bacaa48223e75b23c4b
zephyr <linjie.zhang@outlook.com>
2023-03-19 19:18:39 +0800

    community page header ui

--- src/app/r/[communityName]/Header.tsx
+++ src/app/r/[communityName]/Header.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import { Community } from '../../../../types'
+import Image from 'next/image'
+import { BsReddit } from 'react-icons/bs'
 
-const Header = () => {
-  return <div>Header</div>
+interface Props {
+  community: Community
+}
+const Header = ({ community }: Props) => {
+  return (
+    <div className="h-[146px]">
+      <p className="bg-blue-500 h-[50%]" />
+      <div className="h-[50%] bg-white">
+        <div className="border border-[red] w-[95%] max-w-[860px] mx-auto">
+          <div className="flex">
+            {community.communityImage ? (
+              <Image height="0" width="0" alt="" src="" />
+            ) : (
+              <BsReddit className="text-[64px] text-brand-100 relative -top-3 rounded-[50%] border-4 border-white" />
+            )}
+            <div className="flex py-[10px] px-4 space-x-4">
+              <div>
+                <p className="text-lg font-semibold">
+                  {community.communityName}
+                </p>
+                <p className="text-gray-600 font-sm">
+                  r/{community.communityName}
+                </p>
+              </div>
+              <button
+                className="border border-blue-500 h-[28px] px-6 text-white 
+              rounded-full bg-blue-500 font-semibold hover:bg-blue-600"
+              >
+                Join
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
--- src/app/r/[communityName]/page.tsx
+++ src/app/r/[communityName]/page.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Header from './Header'
+import { doc, getDoc, onSnapshot } from 'firebase/firestore'
+import { db } from '@/firebase'
+import { Community } from '../../../../types'
+import { notFound } from 'next/navigation'
 
-const CommunityPage = () => {
-  return <div>page</div>
+const CommunityPage = async ({
+  params,
+}: {
+  params: { communityName: string }
+}) => {
+  const communityDoc = await getDoc(
+    doc(db, 'communities', params.communityName)
+  )
+
+  const communityData = communityDoc.data()
+  if (!communityData) {
+    notFound()
+  }
+  const community = {
+    communityName: params.communityName,
+    ...communityData,
+  } as Community
+  return (
+    <>
+      <Header community={community} />
+    </>
+  )
 }
 
 export default CommunityPage
