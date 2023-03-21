622dac519e01e6288f36045d162da1d93661037d
zephyr <linjie.zhang@outlook.com>
2023-03-21 18:13:46 +0800

    posts ui

--- src/app/r/[communityName]/PageContent.tsx
+++ src/app/r/[communityName]/PageContent.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const PageContent = ({ children }: { children: React.ReactNode[] }) => {
   return (
     <div className="flex justify-center pt-4">
-      <div className="flex w-[95%] max-w-[860px] space-x-6">
+      <div className="flex w-[95%] max-w-[860px] ">
         {/* left hand */}
-        <div className="flex flex-col sm:w-full md:w-[65%]">{children[0]}</div>
+        <div className="flex flex-col w-[100%] md:w-[65%]">{children[0]}</div>
         {/* right hand */}
-        <div className="flex-col hidden md:flex border-[red] border flex-1">
+        <div className="ml-6 flex-col hidden md:flex border-[red] border flex-1">
           {children[1]}
         </div>
       </div>
--- src/app/r/[communityName]/Posts/PostItem.tsx
+++ src/app/r/[communityName]/Posts/PostItem.tsx
@@ -1,11 +1,63 @@
 import React from 'react'
 import { Post } from '../../../../../types'
+import Image from 'next/image'
+import {
+  BsChat,
+  BsBookmark,
+  BsArrowUpCircle,
+  BsArrowDownCircle,
+} from 'react-icons/bs'
+import { RiShareForwardLine } from 'react-icons/ri'
+import { FiTrash } from 'react-icons/fi'
 
 interface Props {
   post: Post
 }
 const PostItem = ({ post }: Props) => {
-  return <div>{post.title}</div>
+  return (
+    <div className="flex mt-4 border border-blue-500">
+      <div className="bg-gray-50 pt-2 px-3 flex flex-col items-center text-gray-700">
+        <BsArrowUpCircle className="text-[16px] cursor-pointer" />
+        {post.voteStatus}
+        <BsArrowDownCircle className="text-[16px] cursor-pointer" />
+      </div>
+      <div className="p-2 bg-white flex-1">
+        <div className="text-sm">
+          Posted by u/{post.creatorName} 20 hors ago
+        </div>
+        <div className="font-semibold">{post.title}</div>
+        <div className="text-sm">{post.body}</div>
+        {post.postImage && (
+          <Image
+            src={post.postImage}
+            height={460}
+            width={460}
+            className="max-w-[460px] max-h-[460px] p-4"
+            alt=""
+          />
+        )}
+
+        <div className="flex flex-start space-x-4 text-gray-700">
+          <div className="postIcon">
+            <BsChat className="h-4 w-4" />
+            <p className="text-sm">{post.numberOfComments}</p>
+          </div>
+          <div className="postIcon">
+            <RiShareForwardLine className="text-[20px]" />
+            <p className="text-sm">Share</p>
+          </div>
+          <div className="postIcon">
+            <BsBookmark className="text-[16px]" />
+            <p className="text-sm">Save</p>
+          </div>
+          <div className="postIcon">
+            <FiTrash className="h-4 w-4" />
+            <p className="text-sm">Delete</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PostItem
