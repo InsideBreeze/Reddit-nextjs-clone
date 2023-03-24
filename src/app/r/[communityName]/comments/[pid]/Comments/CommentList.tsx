4b40c66313ae1197d73b9a90df200d38f318898a
zephyr <linjie.zhang@outlook.com>
2023-03-24 14:37:34 +0800

    nest comments

--- src/app/r/[communityName]/comments/[pid]/Comments/CommentList.tsx
+++ src/app/r/[communityName]/comments/[pid]/Comments/CommentList.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Comment } from '../../../../../../../types'
 import CommentItem from './CommentItem'
+import { User } from 'firebase/auth'
 
 interface Props {
   comments: Comment[]
+  onDeleteComment: (id: string) => void
+  user?: User | null
 }
-const CommentList = ({ comments }: Props) => {
+const CommentList = ({ comments, onDeleteComment, user }: Props) => {
   return (
     <>
       {comments.map(comment => (
-        <CommentItem comment={comment} key={comment.id} />
+        <CommentItem
+          comment={comment}
+          key={comment.id}
+          onDeleteComment={onDeleteComment}
+          user={user}
+        />
       ))}
     </>
   )
